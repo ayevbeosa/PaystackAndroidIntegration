a8e9e85f7accbfe7dcb4aaf3b5f83e0390517338
Iyamu Joshua <ayevbeosa.j@gmail.com>
2020-05-09 13:18:38 +0100

    Set public key in Activity file.

--- app/src/main/java/com/ayevbeosa/paystackintegration/MainActivity.kt
+++ app/src/main/java/com/ayevbeosa/paystackintegration/MainActivity.kt
@@ -2,11 +2,14 @@ package com.ayevbeosa.paystackintegration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.paystack.android.PaystackSdk
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        PaystackSdk.setPublicKey("YOUR PUBLIC KEY GOES HERE")
     }
 }

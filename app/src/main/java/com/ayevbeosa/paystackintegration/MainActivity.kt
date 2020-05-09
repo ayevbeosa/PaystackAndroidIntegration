c441c6b10aa16f7e60bcf0890e541af46b3fe97d
Iyamu Joshua <ayevbeosa.j@gmail.com>
2020-05-09 13:28:09 +0100

    Create simple validation function

--- app/src/main/java/com/ayevbeosa/paystackintegration/MainActivity.kt
+++ app/src/main/java/com/ayevbeosa/paystackintegration/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ayevbeosa.paystackintegration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.paystack.android.PaystackSdk
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         PaystackSdk.setPublicKey("YOUR PUBLIC KEY GOES HERE")
+    }
+
+    private fun isCardDetailsValid(): Boolean {
+        var valid = true
+
+        val inputCardNumber = card_number_edit_text.text.toString().trim()
+        if (inputCardNumber.isNotEmpty()) {
+            card_number_edit_text_layout.error = null
+        } else {
+            valid = false
+            card_number_edit_text_layout.error = "This field is required"
+        }
+
+        val inputCVC = cvc_edit_text.text.toString().trim()
+        if (inputCVC.isNotEmpty()) {
+            cvc_edit_text_layout.error = null
+        } else {
+            valid = false
+            cvc_edit_text_layout.error = "This field is required"
+        }
+
+        val inputExpMonth = expiry_month_edit_text.text.toString().trim()
+        if (inputExpMonth.isNotEmpty()) {
+            expiry_month_edit_text_layout.error = null
+        } else {
+            valid = false
+            expiry_month_edit_text_layout.error = "This field is required"
+        }
+
+        val inputExpYear = expiry_year_edit_text.text.toString().trim()
+        if (inputExpYear.isNotEmpty()) {
+            expiry_year_edit_text_layout.error = null
+        } else {
+            valid = false
+            expiry_year_edit_text_layout.error = "This field is required"
+        }
+
+        return valid
     }
 }

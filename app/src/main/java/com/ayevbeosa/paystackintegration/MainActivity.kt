ca0ad0067d7f33d0797dc042ab73d0efec7335da
Iyamu Joshua <ayevbeosa.j@gmail.com>
2020-05-09 13:40:53 +0100

    Create function that builds Card object

--- app/src/main/java/com/ayevbeosa/paystackintegration/MainActivity.kt
+++ app/src/main/java/com/ayevbeosa/paystackintegration/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ayevbeosa.paystackintegration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.paystack.android.PaystackSdk
+import co.paystack.android.model.Card
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         PaystackSdk.setPublicKey("YOUR PUBLIC KEY GOES HERE")
+    }
+
+    /**
+     * Function to create new Card class.
+     */
+    private fun createCardObject(): Card {
+        // Get fields
+        val cardNum = card_number_edit_text.text.toString().trim()
+        val expiryMonth = cvc_edit_text.text.toString().trim().toInt()
+        val expiryYear = expiry_year_edit_text.text.toString().trim().toInt()
+        val cvc: String = cvc_edit_text.text.toString().trim()
+
+        return Card.Builder(cardNum, expiryMonth, expiryYear, cvc)
+            .build()
     }
 
     private fun isCardDetailsValid(): Boolean {
